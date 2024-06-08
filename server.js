294e42ab4af8244fdddf7e1b9b75dcdfd0068271
Devi Priya S <114299272+DeviPriyasoundararaj@users.noreply.github.com>
2024-06-08 10:12:52 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const urlRoutes = require('./routes/urlRoutes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/url', urlRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
